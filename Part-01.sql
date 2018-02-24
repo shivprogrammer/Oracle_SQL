bc58178c322f0a040459f105022252892cb96aba
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-24 03:50:38 -0800

    SQL retrival statement

--- Part-01.sql
+++ Part-01.sql
@@ -9,3 +9,7 @@ CREATE TABLE employee
 
 -- employee_id   NUMBER
 -- -> this line creates a table column (data attribute) called EMPLOYEE_ID with a datatype of NUMBER. This EMPLOYEE_ID column is therefore defined to contain only numeric data.
+
+SELECT first_name, last_name, hire_date
+FROM employee;
+-- retrives all FIRST_NAME, LAST_NAME, and HIRE_DATE column values from this article's EMPLOYEE table

34c3dcd4b078013674525105dd36f3c3ed860150
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-24 03:46:54 -0800

    creating employee table in SQL

---
+++ Part-01.sql
@@ -0,0 +1,8 @@
+-- The following statement creates a new EMPLOYEE table
+CREATE TABLE employee
+  (employee_id  NUMBER,
+   first_name   VARCHAR2(30),
+   last_name    VARCHAR2(30),
+   hire_date    DATE,
+   salary       NUMBER(9,2),
+   manager      NUMBER);

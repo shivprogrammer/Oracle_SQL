29c46e708ea4a47ea4a1a711f6a6c5dd60e27817
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-24 03:49:03 -0800

    add elaboration upon a line of table creation

--- Part-01.sql
+++ Part-01.sql
@@ -6,3 +6,6 @@ CREATE TABLE employee
    hire_date    DATE,
    salary       NUMBER(9,2),
    manager      NUMBER);
+
+-- employee_id   NUMBER
+-- -> this line creates a table column (data attribute) called EMPLOYEE_ID with a datatype of NUMBER. This EMPLOYEE_ID column is therefore defined to contain only numeric data.
